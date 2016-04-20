3a7de93fe68d97b66c4b51565fb88b40148c6829
FranciscoAnsaldo <franciscoansaldofiuba@gmail.com>
2016-04-20 18:52:44 -0300

    Logica obtencion de grupos en DeterminarGanadores

--- binarios/DeterminarGanadores.pl
+++ binarios/DeterminarGanadores.pl
@@ -16,6 +16,46 @@ sub obtener_nombre_archivo_sorteo {
       close(PROCDIR);
 }
 
+#Devuelve los nro de grupos abiertos en el arreglo recibido
+sub obtener_grupos {
+	@grupos_por_parametro = grep /^\d{4}$/, @_;
+	my @grupos_en_rango_por_parametro = &obtener_grupos_en_rango(@_);
+	push(@grupos_por_parametro, @grupos_en_rango_por_parametro);
+	my %hash_aux = map{$_,1} @grupos_por_parametro;
+	@grupos_por_parametro = keys %hash;
+	if (scalar @grupos_por_parametro > 0){
+		if (opendir(arch_grupos, "<$ENV{'MAEDIR'}/Grupos.csv")){
+			@grupos_en_arch = readdir(arch_grupos);
+			close(arch_grupos);
+		}
+		@grupos_activos = grep /^\d{4};ABIERTO/, @grupos_en_arch;
+		foreach $grupo (@grupos_por_parametro){
+			@grupo_activo = grep /^$grupo/, @grupos_en_arch;
+			if (scalar @grupo_activo){
+				push(@grupos, $grupo);
+			}
+		}
+	}
+	return @grupos;
+}
+
+#Devuelve todos los grupos dentro de los rangos que tiene el arreglo recibido
+sub obtener_grupos_en_rango {
+	@rango_grupos_por_parametro = grep /^\d{4}-\d{4}$/, @_;
+	foreach $rango (@rango_grupos_por_parametro){
+		$inicio = substr($rango, 0, 4);
+		$fin = substr($rango, 5, 4);
+		for ($i = $inicio; $i <= $fin; $i++){
+			push($grupos, $i);
+		}
+	}
+	return $grupos;
+}
+
+sub presentar_menu {
+	print "menu\n";
+}
+
 sub resultado_general_sorteo {
 	$id = @_[0];
 	my $file = &obtener_nombre_archivo_sorteo($id);
@@ -36,6 +76,7 @@ sub resultado_general_sorteo {
 	close(ENTRADA);
 	return 0;
 }
+
 
 # Verifico q el ambiente este inicializado
 if ($ENV{'AMBIENTE_INICIALIZADO'} ne "SI" ) {
@@ -67,7 +108,7 @@ if (scalar @ayuda > 0) {
 
 
 # Verifico que exista el archivo 
-$id = @ARGV[0];
+$id = shift(@ARGV);
 $archivo = &obtener_nombre_archivo_sorteo($id);
 if ( $archivo eq "") {
 	print "No se encuentra el archvio de sorteo con id: $id\n";
@@ -80,9 +121,15 @@ if (scalar @guardar_en_archivo) {
 	$g = 1;
 }
 
-$opcion="1";
-while ( $opcion != "0" ) {
-	$opcion = <STDIN>;
-	if ($opcion == "1") {&resultado_general_sorteo($id);}
-	
+@grupos = &obtener_grupos(@ARGV);
+
+%opciones = ("resultado_general", 1, "ganadores_sorteo", 2, "ganadores_licitacion", 3, "resultado_por_grupo", 4);
+&presentar_menu;
+while ( $opcion = <STDIN> ) {
+	chop($opcion);
+	if ($opcion == $opciones{resultado_general}) {&resultado_general_sorteo($id);}
+	#else if ($opcion == $opciones{ganadores_sorteo}) {&ganadores_por_sorteo($id, @grupos);}
+	#else if ($opcion == $opciones{ganadores_licitacion}) {&ganadores_por_licitacion($id, @grupos);}
+	#else if ($opcion == $opciones{resultado_por_grupo}) {&resultado_por_grupo($id, @grupos);
+	&presentar_menu;
 }
