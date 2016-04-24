c2d3b6b2e4902a2fd7046ea2abfbcad0a8772d0a
FranciscoAnsaldo <franciscoansaldofiuba@gmail.com>
2016-04-24 16:14:32 -0300

    Logica DeterminarGanadores terminada sin probar, falta menu y menu de ayuda

--- binarios/DeterminarGanadores.pl
+++ binarios/DeterminarGanadores.pl
@@ -16,6 +16,14 @@ sub obtener_nombre_archivo_sorteo {
       return @files[0];
 }
 
+#Recibe el nombre de un archivo de sorteo y su id, y devuelve la fecha de adjudicacion de dicho sorteo
+sub obtener_fecha_de_adjudicacion {
+	my ($fecha_adj, $id) = @_;
+	$fecha_adj =~ s/$id-//;
+	$fecha_adj =~ s/.csv//;
+	return $fecha_adj;
+}
+
 
 #Devuelve los nro de grupos abiertos en el arreglo recibido
 sub obtener_grupos {
@@ -45,17 +53,21 @@ sub obtener_grupos {
 
 		#Saco los grupos que no esten abiertos
 		if (scalar @grupos_por_parametro > 0){
-			if (opendir(GRUPOS, "<$ENV{'MAEDIR'}/Grupos.csv")){
-				@grupos_en_arch = readdir(GRUPOS);
-				close(GRUPOS);
+			my @grupos_activos;
+			open(GRUPOS, "<$ENV{'MAEDIR'}/Grupos.csv");
+			while (my $grupo_existente = <GRUPOS>){
+				$grupo_activo = grep /^\d{4};ABIERTO/, $grupo_existente;
+				if ($grupo_activo ne ""){
+					push(@grupos_activos, $grupo_activo);
+				}
 			}
-			@grupos_activos = grep /^\d{4};ABIERTO/, @grupos_en_arch;
 			foreach $grupo (@grupos_por_parametro){
-				@grupo_activo = grep /^$grupo/, @grupos_en_arch;
+				@grupo_activo = grep /^$grupo/, @grupos_activos;
 				if (scalar @grupo_activo){
 					push(@grupos, $grupo);
 				}
 			}
+			close(GRUPOS);
 		}
 	}
 	return @grupos;
@@ -74,32 +86,21 @@ sub obtener_grupos_en_rango {
 	return @grupos;
 }
 
-
 sub presentar_menu {
 	print "menu\n";
 }
 
-
-sub resultado_general_sorteo {
-	my ($grabar, $id) = @_;
-	my $file = &obtener_nombre_archivo_sorteo($id);
+sub resultado_sorteo {
+	my $file = @_;
 	open (ENTRADA,"<$ENV{'PROCDIR'}/sorteos/$file");
 	my %orden_sorteo_h;
 	while ($linea=<ENTRADA>) {
 		$linea =~ s/\D*(\d*)\D*(\d*)/\2 \1/;
 		@orden_sorteo = split(" ", $linea);
-		$orden_sorteo_h{$orden_sorteo[1]} = $orden_sorteo[0]
-	}
-	@keys = keys %orden_sorteo_h;
-	@sorted_keys = sort(@keys);
-	foreach $a (@sorted_keys) {
-		if ( $a ne "" ) {
-			push(@resultado,"Nro. de Sorteo $a, le correspondió al número de orden $orden_sorteo_h{$a}\n");
-			print $resultado[$#resultado];
-		}
+		$orden_sorteo_h{$orden_sorteo[0]} = $orden_sorteo[1];
 	}
 	close(ENTRADA);
-	return 0;
+	return %orden_sorteo_h;
 }
 
 
@@ -107,21 +108,8 @@ sub ganadores_por_sorteo {
 	my ($id, @grupos) = @_;
 	my $file = &obtener_nombre_archivo_sorteo($id);
 
-	#Armo el titulo
-	my $fecha_adj = $file;
-	$fecha_adj =~ s/$id-//;
-	$fecha_adj =~ s/.csv//;
-	$titulo = "Ganadores del Sorteo ".$id." de fecha ".$fecha_adj."\n";
-	print $titulo;
-
 	#Averiguo orden sorteo
-	open (ENTRADA,"<$ENV{'PROCDIR'}/sorteos/$file");
-	my %orden_sorteo_h;
-	while ($linea=<ENTRADA>) {	
-		$linea =~ s/\D*(\d*)\D*(\d*)/\2 \1/;
-		@orden_sorteo = split(" ", $linea);
-		$orden_sorteo_h{@orden_sorteo[1]} = @orden_sorteo[0]
-	}
+	my %orden_sorteo_h = &resultado_sorteo($file);
 	@orden_keys = keys %orden_sorteo_h;
 	@orden_keys = sort(@keys);
 	close(ENTRADA);
@@ -135,6 +123,7 @@ sub ganadores_por_sorteo {
 		if($matches ne ""){
 			$suscriptores_por_grupo{$info_suscriptor[0]}{$info_suscriptor[1]} = $info_suscriptor[2];
 	}
+	close(SUSCRIPTORES);
 	%ganadores_sorteo_por_grupo;
 	foreach $grupo (@grupos){
 		for(i = 0; not exists($suscriptores_por_grupo{$grupo}{$orden_sorteo{@orden_keys[i]}}) ; i++);
@@ -143,13 +132,146 @@ sub ganadores_por_sorteo {
 	return %ganadores_sorteo_por_grupo;
 }
 
+sub ganadores_por_licitacion{
+	my ($id, @grupos) = @_;
+	$file_sorteo = &obtener_nombre_archivo_sorteo($id);
+	$fecha_adj = &obtener_fecha_de_adjudicacion($file, $id);
+	$file_licitacion = $fecha_adj.".txt";
+	my %ganadores_por_sorteo = &ganadores_por_sorteo($id, @grupos);
+	my %campos_ganadores_sorteo = ("orden", 0, "nombre", 1, "sorteo", 2);
+	my %resultado_sorteo = &resultado_sorteo($file_sorteo);
+
+	my %ganador_licitacion;
+	open(LICITACIONES, "<$ENV{'PROCDIR'}/validas/.$file_licitacion");
+	my %campos_licitacion = ("grupo", 3, "orden", 4, "importe", 5, "nombre", 6, "nro_sorteo", 7);
+	while ($licitacion_existente = <LICITACIONES>){
+		@licitacion = split(";", $licitacion_grupo_buscado);
+		for (i = 0; i < $#grupos && $licitacion[grupo] ne $grupos[i]; i++);
+		if (i < $#grupos && $licitacion[$campos_licitacion{orden}] ne $ganadores_por_sorteo{$grupos[i]}[$campos_ganadores_sorteo{orden}]){
+			foreach $key (keys(%resultado_sorteo)){
+				if ($resultado_sorteo[$key] eq $licitacion[$campos_licitacion{orden}]){
+					$licitacion[$campos_licitacion{nro_sorteo}] = $key;
+					last;
+				}
+			}
+
+			if (not exists($ganador_licitacion{$licitacion[$campos_licitacion{grupo}]}) || $licitacion[$campos_licitacion{importe}] > $ganador_licitacion{$licitacion[$campos_licitacion{grupo}]}[1] || ($licitacion[$campos_licitacion{importe}] == $ganador_licitacion{$licitacion[$campos_licitacion{grupo}]}[1] && $licitacion[$campos_licitacion{nro_sorteo}] > $ganador_licitacion{$licitacion[$campos_licitacion{grupo}]}[3])){
+
+				$ganador_licitacion{$licitacion[$campos_licitacion{grupo}]} = ($licitacion[$campos_licitacion{orden}], $licitacion[$campos_licitacion{importe}], $licitacion[$campos_licitacion{nombre}], $licitacion[$campos_licitacion{nro_sorteo}]);
+
+			}
+		}
+	}
+	close(LICITACIONES);
+	return %ganador_licitacion;
+}
+
+
+sub resultado_general_sorteo {
+	my ($grabar, $id) = @_;
+	my $file = &obtener_nombre_archivo_sorteo($id);
+	my %orden_sorteo_h = &resultado_sorteo($file);
+	@keys = keys %orden_sorteo_h;
+	@sorted_keys = sort(@keys);
+	
+	if ($grabar) {
+		$file =~ s/csv/txt/;
+		open(SALIDA, ">$ENV{'INFODIR'}/$file");
+	}
+	foreach $a (@sorted_keys) {
+		if ( $a ne "" ) {
+			$resultado = "Nro. de Sorteo $a, le correspondió al número de orden $orden_sorteo_h{$a}\n";
+			print $resultado;
+			if ($grabar) {print SALIDA $resultado;}
+		}
+	}
+	if ($grabar) {close(SALIDA);}
+	return 0;
+}
+
 #Administra la obtención de los datos necesarios y la presentación de la opción ganadores por sorteo
 sub resultado_ganadores_por_sorteo{
 	($grabar, $id, @grupos) = @_;
 	my %ganadores_por_grupo = &ganadores_por_sorteo($id, @grupos);
+
+	#Armo el titulo
+	my $fecha_adj = &obtener_fecha_de_adjudicacion($file, $id);
+	$titulo = "Ganadores del Sorteo ".$id." de fecha ".$fecha_adj."\n";
+	print $titulo;
+
+	if ($grabar) {
+		my @grupos_ordenados = sort(@grupos);
+		$arch_salida = $id."_S_Grd".$grupos_ordenados[0]."_Grh".$grupos_ordenados[$#grupos_ordenados]."_".$fecha_adj;
+		open(SALIDA, ">$ENV{'INFODIR'}/$arch_salida");
+	}
 	@resultado;
 	foreach $grupo (@keys(%ganadores_por_grupo){
-		push(@resultado, "Ganador por sorteo del grupo ".$grupo.": Nro de Orden: ".$ganadores_por_grupo{$grupo}[0].", ".$ganadores_por_grupo{$grupo}[1]."(Nro. de Sorteo ".$ganadores_por_grupo{$grupo}[2].")\n");
+		$resultado = "Ganador por sorteo del grupo ".$grupo.": Nro de Orden: ".$ganadores_por_grupo{$grupo}[0].", ".$ganadores_por_grupo{$grupo}[1]."(Nro. de Sorteo ".$ganadores_por_grupo{$grupo}[2].")\n";
+		print $resultado;
+		if ($grabar) {
+			print SALIDA $resultado;
+		}
+	}
+	if ($grabar) {
+		close(SALIDA);
+	}
+	return 0;
+}
+
+#Administra la obtención de los datos necesarios y la presentación de la opción ganadores por sorteo
+sub resultado_ganadores_por_licitacion{
+	($grabar, $id, @grupos) = @_;
+	my %ganadores_por_grupo = &ganadores_por_licitacion($id, @grupos);
+
+	#Armo el titulo
+	my $fecha_adj = &obtener_fecha_de_adjudicacion($file, $id);
+	$titulo = "Ganadores por Licitación ".$id." de fecha ".$fecha_adj."\n";
+	print $titulo;
+
+	if ($grabar) {
+		my @grupos_ordenados = sort(@grupos);
+		$arch_salida = $id."_L_Grd".$grupos_ordenados[0]."_Grh".$grupos_ordenados[$#grupos_ordenados]."_".$fecha_adj;
+		open(SALIDA, ">$ENV{'INFODIR'}/$arch_salida");
+	}
+	@resultado;
+	foreach $grupo (@keys(%ganadores_por_grupo){
+		$resultado = "Ganador por licitación del grupo ".$grupo.": Nro de Orden: ".$ganadores_por_grupo{$grupo}[0].", ".$ganadores_por_grupo{$grupo}[1]." con $".$ganadores_por_grupo{$grupo}[2]."(Nro. de Sorteo ".$ganadores_por_grupo{$grupo}[3].")\n";
+		print $resultado;
+		if ($grabar){
+			print SALIDA $resultado;
+		}
+	}
+	if ($grabar){
+		close(SALIDA);
+	}
+	return 0;
+}
+
+sub resultado_por_grupos{
+	($grabar, $id, @grupos) = @_;
+	my %ganadores_por_sorteo = &ganadores_por_sorteo($id, @grupos);
+	my %ganadores_por_licitacion = &ganadores_por_licitacion($id, @grupos);
+
+	#Armo el titulo
+	my $fecha_adj = &obtener_fecha_de_adjudicacion($file, $id);
+	$titulo = "Ganadores por Grupo en el acto de adjudicación de fecha ".$fecha_adj.", Sorteo: ".$id."\n";
+	print $titulo;
+
+	@resultado;
+	foreach $grupo (@keys(%ganadores_por_sorteo){
+		if ($grabar) {
+			$arch_salida = $id."_Grupo".$grupo."_".$fecha_adj;
+			open(SALIDA, ">$ENV{'INFODIR'}/$arch_salida");
+		}
+		$resultado_sorteo = $grupo."-".$ganadores_por_sorteo{$grupo}[0]." S (".$ganadores_por_sorteo{$grupo}[1].")\n";
+		$resultado_licitacion = $grupo."-".$ganadores_por_licitacion{$grupo}[0]." L (".$ganadores_por_licitacion{$grupo}[1].")\n";
+		print $resultado_sorteo;
+		print $resulotado_licitacion;
+		if ($grabar) {
+			print SALIDA $resultado_sorteo;
+			print SALIDA $resulotado_licitacion;
+			close(SALIDA);
+		}
 	}
 	return 0;
 }
@@ -203,9 +325,9 @@ if (scalar @guardar_en_archivo) {
 &presentar_menu;
 while ( $opcion = <STDIN> ) {
 	chop($opcion);
-	if ($opcion == $opciones{resultado_general}) {&resultado_general_sorteo($g, $id);}
-	else if ($opcion == $opciones{ganadores_sorteo}) {&resultado_ganadores_por_sorteo($g, $id, @grupos);}
-	#else if ($opcion == $opciones{ganadores_licitacion}) {&ganadores_por_licitacion($id, $g, @grupos);}
-	#else if ($opcion == $opciones{resultado_por_grupo}) {&resultado_por_grupo($id, $g, @grupos);
+	if ($opcion eq $opciones{resultado_general}) {&resultado_general_sorteo($g, $id);}
+	elsif ($opcion eq $opciones{ganadores_sorteo}) {&resultado_ganadores_por_sorteo($g, $id, @grupos);}
+	elsif ($opcion eq $opciones{ganadores_licitacion}) {&resultado_ganadores_por_licitacion($g, $id, @grupos);}
+	elsif ($opcion eq $opciones{resultado_por_grupo}) {&resultado_por_grupo($id, $g, @grupos);}
 	&presentar_menu;
 }
